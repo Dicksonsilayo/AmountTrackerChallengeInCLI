7826e5a6eed039fd1e4a74a24c247aa9c5db18d9
Dicksonsilayo <dicksonsilayo515@gmail.com>
2026-06-26 14:33:33 +0300

    initial commit

---
+++ expenses.php
@@ -0,0 +1,163 @@
+<?php
+// require_once 'validations.php';
+class ExpenseTracker{
+private $file="storage.json";
+public function __construct()
+{
+   if(!file_exists($this->file)){
+    $data=file_put_contents($this->file,json_encode([],JSON_PRETTY_PRINT));
+   }
+}
+private function readExpense(){
+    $data=file_get_contents($this->file);
+    return json_decode($data,true);
+
+}
+private function saveExpense($expenses){
+    
+file_put_contents($this->file,json_encode($expenses,JSON_PRETTY_PRINT));
+}
+private function getNextExpense($expenses){
+    if(empty($expenses)){
+        return 1;
+
+    }
+    $ids=array_column($expenses,'id');
+    if(empty($ids)){
+        return 1;
+    }
+    return max(array_column($expenses,'id'))+1;
+
+}
+public function addExpense($description,$amount){
+    $expenses=$this->readExpense();
+$expense=[
+    'id'=>$this->getNextExpense($expenses),
+    'date'=>date('Y-m-d H:i:s'),
+    'description'=>$description,
+    'expense'=>(float)$amount,
+];
+$expenses[]=$expense;
+$this->saveExpense($expenses);
+echo "new expense with {$expense['id']} has been saved successfully";
+
+}
+public function updateExpense($id,$description,$amount)
+{
+    $expenses=$this->readExpense();
+    foreach($expenses as $expense){
+        if($expense['id']==$id){
+            $expense['description']=$description;
+            $expense['expense']=(float)$amount;
+           $this->saveExpense($expense);
+           echo " the expense with {id} has been updated successfully";
+           return;
+
+            
+        }
+    }
+
+    }
+
+
+public function deleteExpense($id){
+    $expenses=$this->readExpense();
+    foreach($expenses as $keys=>$expense){
+if($expense['id']==$id){
+    unset($expenses[$keys]);
+    $this->saveExpense($expenses);
+    echo "the expense has been deleted successfully";
+    return;
+
+}
+    }
+
+
+}
+public function viewAllExpenses(){
+    $expenses=$this->readExpense();
+      echo "belew are list of all the expenses \n \n";
+    foreach($expenses as $expense){
+      
+        echo "'id' => {$expense['id']} \n";
+         echo "'date' => {$expense['date']} \n";
+          echo "'description' => {$expense['description']} \n";
+           echo "'amount' => {$expense['amount']} \n";
+           echo"\n\n-------------------\n\n";
+    }
+
+}
+public function getmonthlyExpenses($month,$year){
+    $expenses=$this->readExpense();
+    $total=0;
+    $filtered=[];
+
+    
+   foreach($expenses as $expense){
+    $expenseDate=strtotime($expense['date']);
+    $expenseMonth=date('m',$expenseDate);
+    $expenseYear=date('Y',$expenseDate);
+if($expenseMonth==$month && $expenseYear==$year){
+    $filtered=$expense;
+    $total+=$expense['expense'];
+
+}
+
+   }
+echo "this monthly total moth expenses is {$total} ";
+
+}
+
+public function filteredExpenses($startDate,$endDate=null){
+    $expenses=$this->readExpense();
+  
+        $start=strtotime($startDate);
+        $end=$endDate ? strtotime($endDate):$start;
+        $found=false;
+        $total=0;
+        foreach($expenses as $expense){
+            $expenseTime=strtotime($expense['date']);
+            if($expenseTime >= $start && $expenseTime<=$end){
+            $found=true;
+            $total+=$expense['expense'];
+           
+
+            }
+             echo " id {$expense['id']} \n";
+              echo " expense {$expense['expense']} \n";
+              echo " date {$expense['date']} \n \n \n ";
+        
+
+
+
+    }
+    echo "the total expenses from {$startDate} to {$endDate} is \t\t {$total}\n ";
+
+
+}
+}
+
+$tracker=new ExpenseTracker;
+$command = $argv['1']?? null;
+switch($command){
+    case "add":
+        $tracker->addExpense($argv[2],$argv[3]);
+        exit;
+        case "update":
+            $tracker->updateExpense($argv[1],$argv[2],$argv[3]);
+            exit();
+            case "delete":
+                $tracker->deleteExpense($argv[2]);
+                exit;
+                case "viewAll":
+                    $tracker->viewAllExpenses($argv[2]);
+                    exit;
+                    case "month":
+                        $tracker->getmonthlyExpenses($argv[2],$argv[3]);
+                        exit;
+case "filterFrom":
+    $tracker->filteredExpenses($argv[2],$argv[3]);
+    exit;
+
+}
+?>
